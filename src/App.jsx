80de69d3a71ca38c9ee69d4b74ababadb54a201c
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-05-22 11:21:04 +0200

    add: milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function App() {
+  const url = "http://localhost:3333/products";
+
+  const [products, setProducts] = useState([]);
+  const [query, setQuery] = useState("");
+
+  // chiamata prodotti cercati
+  useEffect(() => {
+    axios.get(`${url}?search=${query}`)
+      .then(res => setProducts(res.data))
+      .catch(err => console.error(err))
+  }, [query]);
+
   return (
     <>
-      <h1>Test</h1>
+      <h1>Lista prodotti</h1>
+
+      <div className="container">
+        <input
+          type="text"
+          placeholder="Cerca un prodotto..."
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+        />
+
+        {query.trim() !== "" && (
+
+          <ul>
+            {
+              products.map(p => (
+                <li key={p.id}>
+                  <figure>
+                    <img src={p.image} alt={`Image of ${p.name}`} />
+                  </figure>
+
+                  <div className="product-text">
+                    <h3>{p.name}</h3>
+                    <span>{p.price} €</span>
+                  </div>
+                </li>
+              ))
+            }
+          </ul>
+
+        )}
+      </div>
     </>
   )
 }
